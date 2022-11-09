3878d81d3ed348964cd6f917ee4cc5e14c97fae2
Azimuth Dynamics <azidyn@mbp2.lan>
2022-11-09 22:58:33 +0000

    update

--- main.js
+++ main.js
@@ -15,12 +15,14 @@ const API_SECRET = Account.API_SECRET;
 
 const client = new FTXClient( API_KEY, API_SECRET );
 
+const PAGE_LIMIT = 100;
 
-const START_DATE = seconds( 2020, 1, 1); // year, month, day
+const START_DATE = seconds( 2020, 1, 1 ); // year, month, day
 const END_DATE = seconds( 2022, 12, 1 );
 
 // save addresses
-const COINS = ['BTC', 'ETH', 'USDT']
+const COINS_SAVED_ADDRESS = ['BTC', 'ETH', 'USDT']
+const MARKETS_ORDER_HISTORY = ['BTC-PERP', 'ETH-PERP'];
 
 const delay = ms => new Promise( ( resolve, _ ) => setTimeout( () => resolve(), ms ) );
 
@@ -37,15 +39,13 @@ const output = {};
 
     await delay( 1000 );
     
-    print('Getting account snapshots=')
+    print('Getting account snapshot')
     res = await client.getAccount();
     output.account_snapshot = res.result;
 
     await delay( 1000 );
-
-    print('Getting deposit history')
-    res = await client.getDepositHistory({ start_time: START_DATE });
-    output.deposits = res.result;
+    
+    output.deposit_history = await batch_deposit_history();
     
     await delay( 1000 );
 
@@ -62,7 +62,7 @@ const output = {};
     print('Getting saved addresses')
     const saved_addresses = [];
 
-    for ( const coin of COINS ) {
+    for ( const coin of COINS_SAVED_ADDRESS ) {
         res = await client.getSavedAddresses( coin );
         saved_addresses.push({ coin, data: res.result })
         await delay( 500 );
@@ -70,7 +70,76 @@ const output = {};
 
     output.saved_addresses = saved_addresses;
 
+    await delay( 1000 );
+
+    print('Getting order history...')
+    const order_history = [];
+    for ( const market of MARKETS_ORDER_HISTORY ) {
+        print(' => ', market );
+        let data = await batch_order_history( market );
+        order_history.push({ market, data });
+    }
+
+    output.order_history = order_history;
+
     fs.writeFileSync('./output.json', JSON.stringify( output ));
+
+    print('wrote output.json');
+    process.exit(1);
 
 
 })();
+
+
+
+// def get_all_trades(self, market: str, start_time: float = None, end_time: float = None) -> List:
+
+async function batch_deposit_history() {
+
+    let d_start = START_DATE, d_end = END_DATE;
+
+    let deposit_history = [];
+
+    while( true ) {
+        res = await client.getDepositHistory({ start_time: d_start, end_time: d_end, limit: PAGE_LIMIT });
+        const data = res?.result || [];
+
+        if ( !data.length )
+            break;
+
+        deposit_history = deposit_history.concat( data );
+
+        d_end = ( Date.parse( data[ data.length -1 ].time ) / 1000)>>0;
+        
+        await delay( 500 );
+    }
+
+    return deposit_history;
+
+}
+
+
+async function batch_order_history( market ) {
+
+    let d_start = START_DATE, d_end = END_DATE;
+
+    let order_history = [];
+
+    while( true ) {
+        res = await client.getOrderHistory({ market, start_time: d_start, end_time: d_end, limit: PAGE_LIMIT });
+
+        const data = res?.result || [];
+
+        if ( !data.length )
+            break;
+
+        order_history = order_history.concat( data );
+
+        d_end = ( Date.parse( data[ data.length -1 ].createdAt ) / 1000)>>0;
+        
+        await delay( 500 );
+    }
+
+    return order_history;
+
+}
