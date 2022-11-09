645ee9dabbca3224191aea4640b3ae1a474109ba
Azimuth Dynamics <azidyn@mbp2.lan>
2022-11-09 23:44:33 +0000

    enough

--- main.js
+++ main.js
@@ -13,16 +13,18 @@ const API_KEY = Account.API_KEY;
 const API_SECRET = Account.API_SECRET;
 
 
+
+
 const client = new FTXClient( API_KEY, API_SECRET );
 
-const PAGE_LIMIT = 100;
+const PAGE_LIMIT = 5000;
 
-const START_DATE = seconds( 2020, 1, 1 ); // year, month, day
+const START_DATE = seconds( 2022, 11, 1 ); // year, month, day
 const END_DATE = seconds( 2022, 12, 1 );
 
 // save addresses
 const COINS_SAVED_ADDRESS = ['BTC', 'ETH', 'USDT']
-const MARKETS_ORDER_HISTORY = ['BTC-PERP', 'ETH-PERP'];
+// const MARKETS_ORDER_HISTORY = ['BTC-PERP', 'ETH-PERP'];
 
 const delay = ms => new Promise( ( resolve, _ ) => setTimeout( () => resolve(), ms ) );
 
@@ -31,6 +33,14 @@ const output = {};
 (async()=>{ 
 
     let res;
+
+    print('Get all historical balances and positions snapshots')
+    // Might work
+    try { 
+        res = await client.requestWrapper.get('historical_balances/requests');
+        output.historial_balances_snapshots = res.result;
+    } catch ( e ) { print( e ) }
+    
 
     // undocumented endpoint
     print('Getting USD snapshots')
@@ -50,8 +60,7 @@ const output = {};
     await delay( 1000 );
 
     print('Getting withdrawal history')
-    res = await client.getWithdrawalHistory({ start_time: START_DATE });
-    output.withdraws = res.result;
+    output.withdrawals = await batch_withdrawal_history();
 
     await delay( 1000 );
 
@@ -73,14 +82,19 @@ const output = {};
     await delay( 1000 );
 
     print('Getting order history...')
-    const order_history = [];
-    for ( const market of MARKETS_ORDER_HISTORY ) {
-        print(' => ', market );
-        let data = await batch_order_history( market );
-        order_history.push({ market, data });
-    }
+    output.order_history = await batch_order_history();
 
-    output.order_history = order_history;
+    print('Getting trigger history...')
+    output.trigger_history = await batch_trigger_history();
+
+    // if ( GET_LEND_BORROW_HISTORY ) {
+    //     print('Getting lending history [takes a while!]...')
+    //     output.lending_history = await batch_lending_history();
+    //     // print( output.lending_history )
+
+    //     // print('Getting borrow history [takes a while!]...')
+    //     // output.borrow_history = await batch_borrow_history();
+    // }
 
     fs.writeFileSync('./output.json', JSON.stringify( output ));
 
@@ -101,6 +115,7 @@ async function batch_deposit_history() {
     let deposit_history = [];
 
     while( true ) {
+
         res = await client.getDepositHistory({ start_time: d_start, end_time: d_end, limit: PAGE_LIMIT });
         const data = res?.result || [];
 
@@ -118,15 +133,38 @@ async function batch_deposit_history() {
 
 }
 
+async function batch_withdrawal_history() {
 
-async function batch_order_history( market ) {
+    let d_start = START_DATE, d_end = END_DATE;
+
+    let widthdrawal_history = [];
+
+    while( true ) {
+        res = await client.getWithdrawalHistory({ start_time: d_start, end_time: d_end, limit: PAGE_LIMIT });
+        const data = res?.result || [];
+
+        if ( !data.length )
+            break;
+
+            widthdrawal_history = widthdrawal_history.concat( data );
+
+        d_end = ( Date.parse( data[ data.length -1 ].time ) / 1000)>>0;
+        
+        await delay( 500 );
+    }
+
+    return widthdrawal_history;
+
+}
+
+async function batch_order_history(  ) {
 
     let d_start = START_DATE, d_end = END_DATE;
 
     let order_history = [];
 
     while( true ) {
-        res = await client.getOrderHistory({ market, start_time: d_start, end_time: d_end, limit: PAGE_LIMIT });
+        res = await client.getOrderHistory({ start_time: d_start, end_time: d_end, limit: PAGE_LIMIT });
 
         const data = res?.result || [];
 
@@ -143,3 +181,84 @@ async function batch_order_history( market ) {
     return order_history;
 
 }
+
+
+async function batch_trigger_history(  ) {
+
+    let d_start = START_DATE, d_end = END_DATE;
+
+    let order_history = [];
+
+    while( true ) {
+        res = await client.getTriggerOrderHistory({ start_time: d_start, end_time: d_end, limit: PAGE_LIMIT });
+
+        const data = res?.result || [];
+
+        if ( !data.length )
+            break;
+
+        order_history = order_history.concat( data );
+
+        d_end = ( Date.parse( data[ data.length -1 ].createdAt ) / 1000)>>0;
+        
+        await delay( 500 );
+    }
+
+    return order_history;
+
+}
+
+// async function batch_lending_history(  ) {
+
+//     let d_start = START_DATE, d_end = END_DATE;
+
+//     let lend_history = [];
+
+//     while( true ) {
+
+//         // res = await client.getLendingHistory({ start_time: d_start, end_time: d_end, limit: PAGE_LIMIT });
+//         res = await client.requestWrapper.get('spot_margin/lending_history', { start_time: d_start, end_time: d_end, limit: PAGE_LIMIT  });
+
+//         const data = res?.result || [];
+
+//         if ( !data.length )
+//             break;
+
+//         lend_history = lend_history.concat( data );
+
+//         d_end = ( Date.parse( data[0  ].time ) / 1000 );
+
+//         console.log( data[ 0 ].time );
+        
+//         await delay( 500 );
+        
+//     }
+
+//     return lend_history;
+
+// }
+
+// async function batch_borrow_history(  ) {
+
+//     let d_start = START_DATE, d_end = END_DATE;
+
+//     let lend_history = [];
+
+//     while( true ) {
+//         res = await client.get({ start_time: d_start, end_time: d_end, limit: PAGE_LIMIT });
+
+//         const data = res?.result || [];
+
+//         if ( !data.length )
+//             break;
+
+//             lend_history = lend_history.concat( data );
+
+//         d_end = ( Date.parse( data[ data.length -1 ].time ) / 1000)>>0;
+        
+//         await delay( 500 );
+//     }
+
+//     return lend_history;
+
+// }
